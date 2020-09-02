35fe3c4ee4b4caf7c8aa5ae248f75c9ce2bd66b1
guazi <guazi@2dfire.com>
2020-09-02 13:19:02 +0800

    [add] 去掉ARouter和RxJava

--- Lib_Module/src/main/java/com/fairy/module/BaseApplication.kt
+++ Lib_Module/src/main/java/com/fairy/module/BaseApplication.kt
@@ -3,7 +3,6 @@ package com.fairy.module
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
-import com.alibaba.android.arouter.launcher.ARouter
 import com.fairy.lib.utils.getAppVersionCode
 import com.fairy.lib.utils.getAppVersionName
 
@@ -23,7 +22,6 @@ abstract class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         this.initPlatform()
-        this.initARouter()
     }
 
     private fun initPlatform() {
@@ -32,18 +30,6 @@ abstract class BaseApplication : Application() {
             .setContext(this)
             .setVersionName(this.getAppVersionName() ?: "unKnow")
             .setVersionCode(this.getAppVersionCode())
-    }
-
-    private fun initARouter() {
-        //初始化路由ARouter
-        // 这两行必须写在init之前，否则这些配置在init过程中将无效
-        if (debug()) {
-            // 打印日志
-            ARouter.openLog()
-            // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
-            ARouter.openDebug()
-        }
-        ARouter.init(this)
     }
 
     abstract fun debug(): Boolean
