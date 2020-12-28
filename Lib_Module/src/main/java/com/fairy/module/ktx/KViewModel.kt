9311f90c927fc0463811605bdff9322902db35b3
guazi <guazi@2dfire.com>
2020-12-28 19:50:05 +0800

    [add] launch添加LoadState

--- Lib_Module/src/main/java/com/fairy/module/ktx/KViewModel.kt
+++ Lib_Module/src/main/java/com/fairy/module/ktx/KViewModel.kt
@@ -1,9 +1,9 @@
 package com.fairy.module.ktx
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fairy.module.exception.BizException
+import com.fairy.module.ui.vm.BaseAndroidViewModel
 import com.fairy.module.ui.vo.LoadState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
@@ -52,14 +52,12 @@ fun ViewModel.launch(
  * ViewModel 协程拓展函数
  * @param context
  * @param start
- * @param loadStateLvd 加载状态LiveData
  * @param loadingTxt 加载中提示文案
  * @param block 调用接口
  */
-fun ViewModel.launch(
+fun BaseAndroidViewModel.launch(
     context: CoroutineContext = EmptyCoroutineContext,
     start: CoroutineStart = CoroutineStart.DEFAULT,
-    loadStateLvd: MutableLiveData<LoadState>,
     loadingTxt: String? = null,
     block: suspend CoroutineScope.() -> Unit
 ) {
